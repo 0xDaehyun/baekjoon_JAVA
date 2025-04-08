f2d33451f3fd7856e76a6b5a7277ce1962c824c5
LEECODER <leedaehyun11@jbnu.ac.kr>
2025-04-08 09:25:52 +0900

    테스트 커밋

--- p1065/src/Main.java
+++ p1065/src/Main.java
@@ -13,8 +13,23 @@ public class Main {
 
             }
         } // 여기까지는 기본 알고리즘
+// 규칙 1. 회의시간이 끝나는 회의를 기준으로 정렬하기.
+        // 규칙 2. 겹치지 않는 선에서 가장 회의시간이 짧은 회의를 정렬 및 선택한다 .
 
-
+        for (int i = 0; i < testcase; i++) { // 정렬 알고리즘.
+            int save1;
+            int save2;
+            for (int j = i + 1; j < testcase; j++) {
+                if(arr[i][1] > arr[j][1]) {
+                    save1 = arr[i][1];  // save에 이전항을 저장해놓는다.
+                    arr[i][1] = arr[j][1];
+                    arr[j][1] = save1;
+                    save2 = arr[i][0];  // save에 이전항을 저장해놓는다.
+                    arr[i][0] = arr[j][0];
+                    arr[j][0] = save2;
+                }
+            }
+        }
 
 
     }
